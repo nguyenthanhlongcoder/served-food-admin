209ce6ad4d1b6a3762bb44c5d1ad5c29da88e2ad
nguyenthanhlongcoder <nguyenthanhlongcoder@gmail.com>
2021-08-26 20:23:28 +0700

    update

--- promotions/views.py
+++ promotions/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from rest_framework import generics
+from rest_framework.filters import OrderingFilter, SearchFilter
 from promotions import models
 from promotions import serializers
 
 class PromotionList(generics.ListCreateAPIView):
     queryset = models.Promotion.objects.all()
     serializer_class = serializers.PromotionSerializer
-    
+    filter_backends = (SearchFilter, OrderingFilter)
+
 class PromotionDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = models.Promotion.objects.all()
     serializer_class = serializers.PromotionSerializer
