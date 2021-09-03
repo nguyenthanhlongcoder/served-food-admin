53d7614d94cf156e517ce2df37b2a59a0ea1777f
nguyenthanhlongcoder <nguyenthanhlongcoder@gmail.com>
2021-09-03 20:17:23 +0700

    update

--- fcm_devices/views.py
+++ fcm_devices/views.py
@@ -2,10 +2,13 @@ from django.shortcuts import render
 from rest_framework import generics
 from fcm_devices import models
 from fcm_devices import serializers
+from django_filters.rest_framework.backends import DjangoFilterBackend
 
 class FCMDeviceList(generics.ListCreateAPIView):
     queryset = models.FCMDevice.objects.all()
     serializer_class = serializers.FCMDeviceSerializer
+    filter_backends = [DjangoFilterBackend]
+    filter_fields = ['is_active','user']
 
 class FCMDeviceDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = models.FCMDevice.objects.all()
