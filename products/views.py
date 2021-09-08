58771473d44511d73590d3f52f5bde85ca33e66e
nguyenthanhlongcoder <nguyenthanhlongcoder@gmail.com>
2021-09-08 21:49:09 +0700

    update

--- products/views.py
+++ products/views.py
@@ -16,7 +16,7 @@ class ProductList(generics.ListCreateAPIView):
     queryset = models.Product.objects.all()
     serializer_class = serializers.ProductSerializer
     filter_backends = [DjangoFilterBackend]
-    filter_fields = ['is_active','name', 'category','label']
+    filter_fields = ['is_active','id', 'category','label']
 
     
 class ProductDetail(generics.RetrieveUpdateDestroyAPIView):
