38040cbe6a097448d971256c6a5678fee56cda47
nguyenthanhlongcoder <nguyenthanhlongcoder@gmail.com>
2021-09-15 18:07:30 +0700

    update

--- orders/urls.py
+++ orders/urls.py
@@ -4,8 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.OrdertList.as_view()),
     path('<int:pk>', views.OrderDetail.as_view()),
-    path('order_item', views.OrderItemList.as_view()),
-    path('order_item/<int:pk>', views.OrderItemDetail.as_view()),
+    path('order_items', views.OrderItemList.as_view()),
+    path('order_items/<int:pk>', views.OrderItemDetail.as_view()),
     path('create_order', views.OrderCreateList.as_view()),
     path('create_order_item', views.OrderItemCreateList.as_view())
 ]
